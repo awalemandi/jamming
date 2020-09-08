3a8b7f8557a3bc1c2e9bbfe4afdc74652c3359a7
awalemandi <mandi.awale@gmail.com>
2020-09-08 17:10:53 +1000

    updated reidrectUri from netlify

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,7 +2,7 @@
 
 let userAccessToken;
 const clientId = process.env.REACT_APP_API_KEY;
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'https://dazzling-wiles-5b87b7.netlify.app/callback';
 
 const Spotify = {
 	getAccessToken() {
